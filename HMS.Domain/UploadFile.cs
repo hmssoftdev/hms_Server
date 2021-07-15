99d75230db0a0348860c1bf68d7776165b6fdf95
ibrahim <mohdi9003@gmail.com>
2021-07-15 19:06:00 +0530

    upload file class

---
+++ HMS.Domain/UploadFile.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Domain
+{
+   public class UploadFile:BaseEntity
+    {
+   
+        public string Name { get; set; }
+        public IFormFile files { get; set; }
+        
+    }
+}
