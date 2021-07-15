5223ca5aa1ce7b39de6fb67378d558ce5354c5be
ibrahim <mohdi9003@gmail.com>
2021-07-15 18:57:39 +0530

    fileupload

---
+++ HMS.Domain/Model/UploadFile.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Domain.Model
+{
+   public class UploadFile
+    {
+        public int Id { get; set; }
+        public IFormFile files { get; set; }
+        public string Name { get; set; }
+    }
+}
