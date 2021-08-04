951c586e22fc05eaa595fb4b95a6c8f5d1cbe79c
Shaikh Ahmed Raza <shaikhahmedraza9@gmail.com>
2021-08-04 11:52:21 +0530

    invoice

---
+++ Invoicer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Invoicer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
