bb58d649ec78a5c523a6cb90dd7e98e93ee8fcfe
shaikhAhmedRaza <shaikhahmedraza9@gmail.com>
2021-08-29 20:17:07 +0530

    Added order models

---
+++ HMS.Domain/DishOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace HMS.Domain
+{
+    public class DishOrder:BaseEntity
+    {
+        public float DeliveryTotal { get; set; }
+        public float GrossTotal { get; set; }
+        public int ItemCount { get; set; }
+        public float ItemTotal { get; set; }
+        public int AdminId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ HMS.Domain/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace HMS.Domain
+{
+    public class OrderItem : BaseEntity
+    {
+        public int Quantity { get; set; }
+        public int ProductId { get; set; }
+        public float Price { get; set; }
+        public float GstCompliance { get; set; }
+        public float GstPrice { get; set; }
+        public int OrderID { get; set; }
+    }
+}
